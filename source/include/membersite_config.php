6b06ac51b1d2e864b3e3eb179a1cdd0627844c2c
Pravin_Roy <roypravin93@gmail.com>
2018-06-17 12:31:30 +0530

    Db Config and Validation

---
+++ source/include/membersite_config.php
@@ -0,0 +1,24 @@
+<?PHP
+require_once("./include/fg_membersite.php");
+
+$fgmembersite = new FGMembersite();
+
+//Provide your site name here
+$fgmembersite->SetWebsiteName('user11.com');
+
+//Provide the email address where you want to get notifications
+$fgmembersite->SetAdminEmail('roypravin93@gmail.com');
+
+//Provide your database login details here:
+//hostname, user name, password, database name and table name
+//note that the script will create the table (for example, fgusers in this case)
+//by itself on submitting register.php for the first time
+$fgmembersite->InitDB(/*hostname*/'localhost',
+                      /*username*/'prasanth',
+                      /*password*/'p',
+                      /*database name*/'testdb',
+                      /*table name*/'fgusers3');
+
+$fgmembersite->SetRandomKey('qSRcVS6DrTzrPvr');
+
+?>
